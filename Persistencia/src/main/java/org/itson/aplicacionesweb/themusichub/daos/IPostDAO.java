c0994c1df5651cf0a02d1864dafb83d11eebe42a
bortcrab <bortcrab@gmail.com>
2024-10-22 01:00:08 -0700

    Añadí para que el DAO de posts tirara excepciones

--- Persistencia/src/main/java/org/itson/aplicacionesweb/themusichub/daos/IPostDAO.java
+++ Persistencia/src/main/java/org/itson/aplicacionesweb/themusichub/daos/IPostDAO.java
@@ -7,15 +7,16 @@ package org.itson.aplicacionesweb.themusichub.daos;
 import java.util.List;
 import org.itson.aplicacionesweb.themusichub.modelo.Comun;
 import org.itson.aplicacionesweb.themusichub.modelo.Post;
+import org.itson.aplicacionesweb.themusichub.persistenciaException.PersistenciaException;
 
 /**
  *
  * @author Familia
  */
 public interface IPostDAO {
-    public Post obtenerPostPorID(Long id);
-    public List<Post> obtenerTodosPosts();
-    public void publicarPost(Post post);
-    public void editarPost(Post post);
-    public void eliminarPost(Post post);
+    public Post obtenerPostPorID(Long id) throws PersistenciaException;
+    public List<Post> obtenerTodosPosts() throws PersistenciaException;
+    public void publicarPost(Post post) throws PersistenciaException;
+    public void editarPost(Post post) throws PersistenciaException;
+    public void eliminarPost(Post post) throws PersistenciaException;
 }
--- Persistencia/src/main/java/org/itson/aplicacionesweb/themusichub/daos/PostDAO.java
+++ Persistencia/src/main/java/org/itson/aplicacionesweb/themusichub/daos/PostDAO.java
@@ -8,11 +8,13 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import javax.persistence.PersistenceException;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import org.itson.aplicacionesweb.themusichub.conexion.IConexion;
 import org.itson.aplicacionesweb.themusichub.modelo.Post;
+import org.itson.aplicacionesweb.themusichub.persistenciaException.PersistenciaException;
 
 /**
  * Clase que implementa la interfaz IPostDAO y define los métodos para realizar
@@ -43,7 +45,7 @@ public class PostDAO implements IPostDAO {
      * @return El post si se encuentra, null si no se encuentra.
      */
     @Override
-    public Post obtenerPostPorID(Long id) {
+    public Post obtenerPostPorID(Long id) throws PersistenciaException {
         // Creamos un entity manager.
         EntityManager em = conexion.crearConexion();
 
@@ -73,6 +75,8 @@ public class PostDAO implements IPostDAO {
             logger.log(Level.INFO, "Se ha consultado la tabla 'posts' y no se obtuvieron resultados.");
 
             return null;
+        } catch (PersistenceException pe) {
+            throw new PersistenciaException("No se pudo consultar obtener el post.");
         } finally {
             // Cerramos el entity manager.
             em.close();
@@ -85,39 +89,43 @@ public class PostDAO implements IPostDAO {
      * @return Lista con todos los posts.
      */
     @Override
-    public List<Post> obtenerTodosPosts() {
+    public List<Post> obtenerTodosPosts() throws PersistenciaException {
         // Creamos un entity manager.
         EntityManager em = conexion.crearConexion();
 
-        // Construimos una instancia de CriteriaBuilder.
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        // Creamos un objeto CriteriaQuery para indicar el resultado de la consulta.
-        CriteriaQuery<Post> cq = cb.createQuery(Post.class);
-        // Creamos una instancia del tipo Root para indicar de qué entidad
-        // se hará la consulta.
-        Root<Post> root = cq.from(Post.class);
+        try {
+            // Construimos una instancia de CriteriaBuilder.
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            // Creamos un objeto CriteriaQuery para indicar el resultado de la consulta.
+            CriteriaQuery<Post> cq = cb.createQuery(Post.class);
+            // Creamos una instancia del tipo Root para indicar de qué entidad
+            // se hará la consulta.
+            Root<Post> root = cq.from(Post.class);
 
-        // Con esta línea especificamos que la consulta seleccionará todos los
-        // campos de Post.
-        cq.select(root);
+            // Con esta línea especificamos que la consulta seleccionará todos los
+            // campos de Post.
+            cq.select(root);
 
-        // Obtenemos la lista de posts.
-        List<Post> listaPosts = em.createQuery(cq).getResultList();
+            // Obtenemos la lista de posts.
+            List<Post> listaPosts = em.createQuery(cq).getResultList();
 
-        // Obtenemos la cantidad de resultados.
-        int i = 0;
-        for (Post post : listaPosts) {
-            i++;
+            // Obtenemos la cantidad de resultados.
+            int i = 0;
+            for (Post post : listaPosts) {
+                i++;
+            }
+
+            // Imprimimos un mensaje de que se ejecutó una consulta.
+            logger.log(Level.INFO, "Se ha consultado la tabla 'tarifas_licencia' y se obtuvieron " + i + " resultados.");
+            // Retornamos la lista de posts.
+            return listaPosts;
+        } catch (PersistenceException pe) {
+            throw new PersistenciaException("No se pudieron consultar todos los posts.");
+        } finally {
+            // Cerramos el entity manager.
+            em.close();
         }
 
-        // Imprimimos un mensaje de que se ejecutó una consulta.
-        logger.log(Level.INFO, "Se ha consultado la tabla 'tarifas_licencia' y se obtuvieron " + i + " resultados.");
-
-        // Cerramos el entity manager.
-        em.close();
-
-        // Retornamos la lista de posts.
-        return listaPosts;
     }
 
     /**
@@ -126,22 +134,26 @@ public class PostDAO implements IPostDAO {
      * @param post Post a insertar.
      */
     @Override
-    public void publicarPost(Post post) {
+    public void publicarPost(Post post) throws PersistenciaException {
         // Creamos un entity manager.
         EntityManager em = conexion.crearConexion();
 
-        // Iniciamos la transacción.
-        em.getTransaction().begin();
+        try {
+            // Iniciamos la transacción.
+            em.getTransaction().begin();
 
-        // Mandamos a persistir el post.
-        em.persist(post);
+            // Mandamos a persistir el post.
+            em.persist(post);
 
-        // Hacemos el commit y cerramos el entity manager.
-        em.getTransaction().commit();
-        em.close();
+            // Hacemos el commit y cerramos el entity manager.
+            em.getTransaction().commit();
+            em.close();
 
-        // Imprimimos un mensaje de que se registró un post.
-        logger.log(Level.INFO, "Se ha insertado 1 post correctamente.");
+            // Imprimimos un mensaje de que se registró un post.
+            logger.log(Level.INFO, "Se ha insertado 1 post correctamente.");
+        } catch (PersistenceException pe) {
+            throw new PersistenciaException("No se pudo insertar el post.");
+        }
     }
 
     /**
@@ -150,22 +162,26 @@ public class PostDAO implements IPostDAO {
      * @param post Post a actualizar.
      */
     @Override
-    public void editarPost(Post post) {
+    public void editarPost(Post post) throws PersistenciaException {
         // Creamos un entity manager.
         EntityManager em = conexion.crearConexion();
 
-        // Iniciamos la transacción.
-        em.getTransaction().begin();
+        try {
+            // Iniciamos la transacción.
+            em.getTransaction().begin();
 
-        // Mandamos a actualizar el post.
-        em.merge(post);
+            // Mandamos a actualizar el post.
+            em.merge(post);
 
-        // Hacemos el commit y cerramos el entity manager.
-        em.getTransaction().commit();
-        em.close();
+            // Hacemos el commit y cerramos el entity manager.
+            em.getTransaction().commit();
+            em.close();
 
-        // Imprimimos un mensaje de que se actualizó una post.
-        logger.log(Level.INFO, "Se ha actualizado 1 post correctamente.");
+            // Imprimimos un mensaje de que se actualizó una post.
+            logger.log(Level.INFO, "Se ha actualizado 1 post correctamente.");
+        } catch (PersistenceException pe) {
+            throw new PersistenciaException("No se pudo editar el post.");
+        }
     }
 
     /**
@@ -174,24 +190,28 @@ public class PostDAO implements IPostDAO {
      * @param post Post a eliminar.
      */
     @Override
-    public void eliminarPost(Post post) {
+    public void eliminarPost(Post post) throws PersistenciaException {
         // Creamos un entity manager.
         EntityManager em = conexion.crearConexion();
 
-        // Iniciamos la transacción.
-        em.getTransaction().begin();
-        
-        post = em.merge(post);
-               
-        // Mandamos a eliminar el post.
-        em.remove(post);
+        try {
+            // Iniciamos la transacción.
+            em.getTransaction().begin();
 
-        // Hacemos el commit y cerramos el entity manager.
-        em.getTransaction().commit();
-        em.close();
+            post = em.merge(post);
 
-        // Imprimimos un mensaje de que se eliminó un post.
-        logger.log(Level.INFO, "Se ha eliminado 1 post correctamente.");
+            // Mandamos a eliminar el post.
+            em.remove(post);
+
+            // Hacemos el commit y cerramos el entity manager.
+            em.getTransaction().commit();
+            em.close();
+
+            // Imprimimos un mensaje de que se eliminó un post.
+            logger.log(Level.INFO, "Se ha eliminado 1 post correctamente.");
+        } catch (PersistenceException pe) {
+            throw new PersistenciaException("No se pudo eliminar el post.");
+        }
     }
 
 }
