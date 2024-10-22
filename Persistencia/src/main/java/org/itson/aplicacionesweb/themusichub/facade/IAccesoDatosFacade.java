c7918d29a001ef0a42e0bc6540aab751364b4a5a
imnotrichi <116768255+imnotrichi@users.noreply.github.com>
2024-10-21 22:20:51 -0700

    Creación de la fachada

--- Persistencia/src/main/java/org/itson/aplicacionesweb/themusichub/facade/IAccesoDatosFacade.java
+++ Persistencia/src/main/java/org/itson/aplicacionesweb/themusichub/facade/IAccesoDatosFacade.java
@@ -8,6 +8,16 @@ package org.itson.aplicacionesweb.themusichub.facade;
  */
 public interface IAccesoDatosFacade {
     
-    public 
+    public void registrarUsuario(UsuarioDTO usuario);
+    
+    public void publicarPost(PostDTO post);
+    
+    public void eliminarPost(PostDTO post);
+    
+    public void comentarPost(ComentarioDTO comentario);
+    
+    public void eliminarComentario(ComentarioDTO comentario);
+    
+    public void responderComentario(ComentarioDTO respuesta);
     
 }
